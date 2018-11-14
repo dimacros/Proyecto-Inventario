9b9736ab22da749ebcf07691db3dc32b3dcf1fd4
AliagaMatuk <aaron.aliaga.matuk@hotmail.com>
2018-11-14 04:03:22 -0500

    Actualización campos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(CustomersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(InvoicesTableSeeder::class);
+        $this->call(InvoiceDetailsTableSeeder::class);
     }
 }
