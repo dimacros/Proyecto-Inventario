bb13fd3f49851211c1d931f167484377b19807fd
AliagaMatuk <aaron.aliaga.matuk@hotmail.com>
2018-11-14 01:09:56 -0500

    Tabla Categories terminada

--- database/migrations/2018_11_14_055723_create_invoices_table.php
+++ database/migrations/2018_11_14_055723_create_invoices_table.php
@@ -15,7 +15,22 @@ class CreateInvoicesTable extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('customer_id')->unsigned()->nullable();
+            $table->string('created_at');
+            $table->string('updated_at');
+            $table->decimal('discount');
+            $table->decimal('sub_total');
+            $table->string('status');
+            $table->string('note');
+            $table->char('user_id')->unsigned()->nullable();
             $table->timestamps();
+
+            $table->foreign('customer_id')
+                  ->references('id')->on('customers')
+                  ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
